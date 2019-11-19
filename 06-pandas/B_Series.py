88ecb88789091adcd00db98250761f7c380353d2
AlexaVane <alexavanemacas321@gmail.com>
2019-11-19 08:56:26 -0500

    pandas

---
+++ 06-pandas/B_Series.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 07:58:07 2019
+
+@author: USRBET
+"""
+
+import numpy as np
+import pandas as pd
+
+lista_numeros = [1,2,3,4]
+tupla_numeros = (1,2,3,4)
+np_numeros = np.array((1,2,3,4))
+
+serie_a = pd.Series(
+        lista_numeros
+        )
+serie_b = pd.Series(
+        tupla_numeros
+        )
+serie_c = pd.Series(
+        np_numeros
+        )
+series_d = pd.Series([
+        True,
+        False,
+        12,
+        12.2,
+        "Ale",
+        None,
+        (),
+        [],
+        {"nombre": "Ale"}
+        ])
+serie_d[3]
+lista_ciudades = ["Ambato", 
+                  "cuenca",
+                  "loja", 
+                  "quito"
+                  ]
+serie_ciudades = pd.Series(
+        lista_ciudades,
+        index = [
+                "a",
+                "c",
+                "l",
+                "q",
+                ])
+serie_ciudades["q"]
+serie_ciudades[3]
+
+valores_ciudad = {
+        "ibarra":9500,
+        "guayaquil":10000,
+        "cuenca":7000,
+        "loja":800
+        }
+serie_valor_ciudad = pd.Series(valores_ciudad)
+serie_valor_ciudad[0]
+
+menor_5000 = serie_valor_ciudad < 5000
+
+serie_valor_ciudad[menor_5000]
+
+s5 = serie_valor_ciudad[menor_5000]
+
+serie_valor_ciudad = serie_valor_ciudad*1.10
+
+serie_valor_ciudad["loja"] = serie_valor_ciudad - 50
+
+print("Lima" in serie_valor_ciudad )
+
+print("loja" in serie_valor_ciudad )
+
+serie_valor_ciudad = np.square(serie_valor_ciudad)
+serie_valor_ciudad = np.sin(serie_valor_ciudad)
+
+ciudades_uno = pd.Series({
+        "Montañita":300,
+        "Gye": 1000,
+        "Quito":2000
+        })
+
+ciudades_dos = pd.Series({
+        "Loja":3000,
+        "Gye": 1000
+        })
+
+print(ciudades_uno + ciudades_dos)
+
+ciudad_add = ciudades_uno.add(ciudades_dos)
+ciudad_concat = pd.concat([
+        ciudades_uno,
+        ciudades_dos])
+ciudad_concat_v = pd.concat([
+        ciudades_uno,
+        ciudades_dos], verify_integrity = True
+)
+
+
+
+
+
+
---
+++ 06-pandas/temp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+nombre = "Ale"
+numero = 3
+lista = [1,2,3]
+tupla = (1,2,3)
+diccionario = {
+        "nombre":"A",
+        "cedula": "1",
+        }
