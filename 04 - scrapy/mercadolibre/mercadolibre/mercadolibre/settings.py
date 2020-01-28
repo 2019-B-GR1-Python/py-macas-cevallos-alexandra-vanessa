70feab00046c62917d60d3c4f27f45e990c04b7b
AlexaVane <alexavanemacas321@gmail.com>
2020-01-28 08:47:01 -0500

    n

--- 04 - scrapy/mercadolibre/mercadolibre/mercadolibre/settings.py
+++ 04 - scrapy/mercadolibre/mercadolibre/mercadolibre/settings.py
@@ -64,10 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'mercadolibre.pipelines.MercadolibrePipeline': 300,
-#}
-
+ITEM_PIPELINES = {
+    ##'mercadolibre.pipelines.MercadolibrePipeline': 300,
+}
+FEED_FORMAT = 'csv'
+FEED_URI = 'tmp/mercadolibre.csv'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
